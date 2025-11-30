de888077a64cb54594f22e876f31a458bef3ae4f
Raviteja <130926923+raviteja-gadige@users.noreply.github.com>
2025-11-30 17:08:34 +0530

    Understanding State and Binding variables

--- StateExample/StateExample/ContentView.swift
+++ StateExample/StateExample/ContentView.swift
@@ -8,18 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var counter: Int = 0
-    
-    var body: some View {
-        VStack {
-           Text("count = \(counter)")
-            Button("Increment") {
-                counter += 1
-            }
-            .padding()
-        }
+  @State private var selectedColor = Color.red
+
+  var body: some View {
+    VStack {
+      Rectangle()
+        .fill(selectedColor)
+        .frame(width: 100, height: 100, alignment: .center)
+
+      ColorPickerView(selectedColor: $selectedColor)
     }
+    .padding()
+  }
 }
+
+struct ColorPickerView: View {
+  @Binding var selectedColor: Color
+
+  let colors: [Color] = [.red, .green, .blue, .yellow, .orange]
+
+  var body: some View {
+    HStack {
+      ForEach(colors, id: \.self) { color in
+        Rectangle()
+          .fill(color)
+          .frame(width: 50, height: 50)
+          .onTapGesture {
+            selectedColor = color
+          }
+      }
+    }
+  }
+}
+
+
 
 struct ContentView_previews: PreviewProvider {
     static var previews: some View {
